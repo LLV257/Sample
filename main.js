4fd4a55cd24b2bec44b319c47fb3768fb400c80e
Aaron Nithin <120745946+LLV257@users.noreply.github.com>
2022-12-28 03:35:50 -0500

    commit2

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 //this is a comments//
 /* sample text */
 
+
 const a = "identifier";
 console.log (a)
+
+
 
 // Whole-script strict mode syntax
 "use strict";
 const v = "Hi! I'm a strict mode script!";
+
+
 
 //javascrpt variables
 let x;
 x = 24;
 console.log(x)
 
+
+
 //javascript string
 let string;
 string="test123";
 console.log(string);
+
+
 
 //strings example
 const name = 'Bob';
@@ -24,8 +33,87 @@ const name1 = "Jack";
 const result = `The names are ${name} and ${name1}`;
 console.log(result);
 
+
+
+
 //javascript is Case-sensitive
 const b = "a";
 const c = "A";
 console.log(b===c);
+
+
+
+
+//javascript undefined 
+let counter;
+console.log(counter);
+
+
+
+
+//Javascript Null 
+let number="null";
+console.log(number);
+
+
+
+
+//Javascript Boolean
+let firstNum = 10;
+let secondNum =5;
+let isGreater = firstNum>secondNum;
+console.log(isGreater);
+
+
+
+
+//Javascript Number
+
+let K = "10";
+console.log(K);
+
+
+
+//Javascript Unary Operator
+
+let j = 90;
+let k = +j;
+
+console.log(k); // 10
+
+
+
+//Javascript Bitwise AND Operator
+let l = 12; 
+let  z = 25; 
+value = l & z; 
+console.log(value); 
+
+
+
+//javascript boolean operator
+let n= true,
+m = false;
+console.log(n || m);
+
+
+
+
+//Javascript Multiplicative operator
+let D = 497;
+D*= 2;
+console.log(D);
+
+
+
+//Javascript Additive Operator 
+let X =30;
+X += 10;
+console.log(X);
+
+console.log("hello" + "everyone");
+
+
+//Javascript Relation operator
+
 
