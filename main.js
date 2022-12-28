1025163398706b2dc5805648f83b730989005a95
Aaron Nithin <120745946+LLV257@users.noreply.github.com>
2022-12-28 00:39:18 -0500

    second commit

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-//this is a comment//
+//this is a comments//
 /* sample text */
+
 const a = "identifier";
 console.log (a)
 
 // Whole-script strict mode syntax
 "use strict";
 const v = "Hi! I'm a strict mode script!";
+
+//javascrpt variables
+let x;
+x = 24;
+console.log(x)
+
+//javascript string
+let string;
+string="test123";
+console.log(string);
+
+//strings example
+const name = 'Bob';
+const name1 = "Jack";
+const result = `The names are ${name} and ${name1}`;
+console.log(result);
+
+//javascript is Case-sensitive
+const b = "a";
+const c = "A";
+console.log(b===c);
+
